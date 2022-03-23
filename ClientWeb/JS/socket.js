f869bbf035c1ce86b72494e400c6e85126011092
Woulfty <70692837+Woulfty@users.noreply.github.com>
2022-03-23 18:28:44 +0100

    modification structure

--- ClientWeb/JS/socket.js
+++ ClientWeb/JS/socket.js
@@ -1,90 +1,73 @@
-const { create } = require("domain");
-
-//var socket = io("ws://192.168.65.44:3000");
-const ws = new WebSocket("ws://192.168.64.183:40510");
-
+//connexion du socket au serveur
+const ws = new WebSocket("ws://192.168.65.44:40510");
+//récupération de la barre de navigation
+var navigation = document.getElementById('navigation');
+    navigation.style.display = "none";
+//récupération du loarder
+var dloader = document.getElementById('dloader');
+    dloader.style.display = "block";
+//Erreur du socket
+ws.addEventListener('error', function (event) {
+    navigation.style.display = "none";
+    dloader.style.display = "block";
+    console.log('WebSocket error: ', event);
+});
+//Quand le socket c'est connecter
 ws.onopen = function () {
     console.log('websocket is connected ...')
     ws.send('connected')
+    //loader
+    dloader = document.getElementById('dloader');
+    dloader.style.display = "none";
+    //barre de navigation
+    navigation = document.getElementById('navigation');
+    navigation.style.display = "block";
 }
-
-ws.onerror = function (error) {
-    console.log(error);
-};
-    
-//affichage des div
-//PV
-var divpv = document.getElementById('dpv');
-    divpv.remove();
-//téléversement ddocument
-var divdoc = document.getElementById('ddoc');
-    divdoc.remove();
-//déconnexion
-var divdeco = document.getElementById('ddeco');
-    divdeco.remove();
-
-//affichage des boutton
-//PV
-var bouttonpv = document.getElementById('bpv');
-    bouttonpv.remove();
-//Téléversement document
-var bouttondoc = document.getElementById('bdoc');
-    bouttondoc.remove();
-//déconnexion
-var bouttondeco = document.getElementById('bdeco');
-    bouttondeco.remove();
-
+//récupération des valeurs dans les champs
 var form = document.getElementById('form');
 var pseudo = document.getElementById('pseudo');
 var mdp = document.getElementById('mdp');
-
+//envoie des données au serveurs
 form.addEventListener('submit', function(e) {
-
     e.preventDefault();
-    if (pseudo.value || mdp.value) {
+    if (pseudo.value && mdp.value) {
         ws.send('UserConnexion', pseudo.value , mdp.value);
         pseudo.value = '';
         mdp.value = '';
     }else{
-
+        window.alert("veuillez remplir les champs");
     }
 });
-
+//récéption de la demande de connexion
 ws.on("ConnectionTrue", (arg) => {
     if (arg.length > 0){
         console.log(arg[0].idUser);
         //création du cookie
         document.cookie = arg[0].idUser;'path=/; expires=' + date;
+        //on cache la div de connexion
+        var dconnexion = document.getElementById('dconnexion');
+        dconnexion.style.display = "none";
+        //apparition de la div de déconnexion
+        var bdeco = document.getElementById('bdeco');
+        bdeco.style.display = "block";
+        //apparition de la div de visualisation des Affaires
+        var bpv = document.getElementById('bpv');
+        bpv.style.display = "block";    
+        //apparition de la div de téléverement de fichier
+        var ddoc = document.getElementById('ddoc');
+        ddoc.style.display = "block";
 
-        //Page de connexion
-        var dconnexion = document.getElementById('ddoc');
-        dconnexion.remove();
+        //demande des affaires au serveur
+        ws.send('ListAffaires');
+    }else{
+        window.alert("Identifiants incorectes");
     }
 });
 
-bdeco.addEventListener('submit', function(e){
-    e.preventDefault();
-    //suppression du cookie
-    document.cookie = 'expires=Thu, 01 Jan 1970 00:00:00 UTC';
-    //affichage des div
-    //PV
-    var divpv = document.getElementById('dpv');
-    divpv.remove();
-    //téléversement ddocument
-    var divdoc = document.getElementById('ddoc');
-    divdoc.remove();
-    //déconnexion
-    var divdeco = document.getElementById('ddeco');
-    divdeco.remove();
-
-    //affichage des boutton
-    //PV
-    var bouttonpv = document.getElementById('bpv');
-    bouttonpv.remove();
-    //Téléversement document
-    var bouttondoc = document.getElementById('bdoc');
-    bouttondoc.remove();
-    //déconnexion
-    var bouttondeco = document.getElementById('bdeco');
-    bouttondeco.remove();
-});
+ws.on("ListAffaires", (arg) => {
+    if (arg.length > 0){
+        
+    }else{
+        window.alert("Une erreur est survenue lors de la récupération des informations... Veuillez réessayer plus tard.");
+    }
+})
