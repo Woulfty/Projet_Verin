2d08d2a90824b976d2339819219b75ef9203d50e
Woulfty <70692837+Woulfty@users.noreply.github.com>
2022-03-23 10:29:55 +0100

    ajout de la déconexion

--- ClientWeb/JS/socket.js
+++ ClientWeb/JS/socket.js
@@ -8,6 +8,10 @@ ws.onopen = function () {
     ws.send('connected')
 }
 
+ws.onerror = function (error) {
+    console.log(error);
+};
+    
 //affichage des div
 //PV
 var divpv = document.getElementById('dpv');
@@ -50,10 +54,37 @@ ws.on("ConnectionTrue", (arg) => {
     if (arg.length > 0){
         console.log(arg[0].idUser);
         //création du cookie
-        document.cookie = arg[0].idUser;
+        document.cookie = arg[0].idUser;'path=/; expires=' + date;
 
         //Page de connexion
         var dconnexion = document.getElementById('ddoc');
         dconnexion.remove();
     }
+});
+
+bdeco.addEventListener('submit', function(e){
+    e.preventDefault();
+    //suppression du cookie
+    document.cookie = 'expires=Thu, 01 Jan 1970 00:00:00 UTC';
+    //affichage des div
+    //PV
+    var divpv = document.getElementById('dpv');
+    divpv.remove();
+    //téléversement ddocument
+    var divdoc = document.getElementById('ddoc');
+    divdoc.remove();
+    //déconnexion
+    var divdeco = document.getElementById('ddeco');
+    divdeco.remove();
+
+    //affichage des boutton
+    //PV
+    var bouttonpv = document.getElementById('bpv');
+    bouttonpv.remove();
+    //Téléversement document
+    var bouttondoc = document.getElementById('bdoc');
+    bouttondoc.remove();
+    //déconnexion
+    var bouttondeco = document.getElementById('bdeco');
+    bouttondeco.remove();
 });
