7d526e57ad03f17db0812ccad2610ab936ec2434
Piebleu <napiebleu@gmail.com>
2022-03-24 12:35:39 +0100

    Avancement Module Test

--- ModulesTest/TestNodeJS/socket.js
+++ ModulesTest/TestNodeJS/socket.js
@@ -1,21 +1,13 @@
 var ws = new WebSocket("ws://192.168.65.44:40510");
-
 ws.onopen = function () {
     console.log('websocket is connected ...')
     ws.send('connected')
 }
-
 ws.onmessage = function (ev) {
     console.log(ev);
 }
 
-var form = document.getElementById('form');
-var pseudo = document.getElementById('pseudo');
-var mdp = document.getElementById('mdp');
+
 form.addEventListener('submit', function(e) {
-    e.preventDefault();
-    if (pseudo.value || mdp.value) {
-        ws.send('UserConnexion', pseudo.value , mdp.value);
-    }else{
-    }
+
 });
--- ModulesTest/TestNodeJS/ws.js
+++ ModulesTest/TestNodeJS/ws.js
@@ -1,27 +1,27 @@
 // Création WebSocketServer
-var WebSocketServer = require('ws').Server;
-wss = new WebSocketServer({port: 40510});
+    var WebSocketServer = require('ws').Server;
+    wss = new WebSocketServer({port: 40510});
 
 // Lancement WebSocketServer
-wss.on('connection', function (ws){
-    // News Connexion
-        console.log("Nouvelle connexion");
-        ws.send("Connexion établie.");
-    // Lancement Date loop for 10 Secondes
-        setInterval(() =>
-            ws.send(`${new Date()}`)
-        ,10 * 1000)
-    // Message Reçu
-        ws.on('message', function (message) {
-            console.log('received: %s', message)
-        })
-    // Request UserConnexion
-        ws.on('UserConnexion', (pseudo, mdp) => {
-            console.log('Pseudo: ' + pseudo);
-            console.log('Mot de passe: ' + mdp);
-            var result = "1";
-            if(pseudo == "woulfty" || mdp == "123"){
-                ws.send("ConnexionTrue", result)
-            }
-        });
-})
+    wss.on('connection', function (ws){
+        // News Connexion
+            console.log("Nouvelle connexion");
+            ws.send("Connexion établie.");
+        // Lancement Date loop for 10 Secondes
+            setInterval(() =>
+                ws.send(`${new Date()}`)
+            ,10 * 1000)
+        // Message Reçu
+            ws.on('message', function (message) {
+                console.log('received: %s', message)
+            })
+        // Request UserConnexion
+            ws.on('UserConnexion', (pseudo, mdp) => {
+                console.log('Pseudo: ' + pseudo);
+                console.log('Mot de passe: ' + mdp);
+                var result = "1";
+                if(pseudo == "woulfty" || mdp == "123"){
+                    ws.send("ConnexionTrue", result)
+                }
+            });
+    })
