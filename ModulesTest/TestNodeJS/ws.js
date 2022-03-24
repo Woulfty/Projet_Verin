d2992fe67684f02f99cbda055e828e93fd1a859b
Piebleu <napiebleu@gmail.com>
2022-03-24 09:58:10 +0100

    Retrait fichier Inutile. Mise à jours WS Module Test.

--- ModulesTest/TestNodeJS/ws.js
+++ ModulesTest/TestNodeJS/ws.js
@@ -1,27 +1,27 @@
-var WebSocketServer = require('ws').Server,
-  wss = new WebSocketServer({port: 40510})
+// Création WebSocketServer
+var WebSocketServer = require('ws').Server;
+wss = new WebSocketServer({port: 40510});
 
-wss.on('connection', function (ws) {
-  console.log("Nouvelle conexion");
-  ws.send("Connecxion établie.");
-
-  ws.on('message', function (message) {
-    console.log('received: %s', message)
-  })
-
-  setInterval(
-      () => ws.send(`${new Date()}`),
-    1000
-  )
-
-  ws.on('UserConnexion', (pseudo, mdp) => {
-    console.log('Pseudo: ' + pseudo);
-    console.log('Mot de passe: ' + mdp);
-
-    var result = "1";
-
-    if(pseudo == "woulfty" || mdp == "123"){
-      ws.send("ConnectionTrue", result)
-    }
-  });
+// Lancement WebSocketServer
+wss.on('connection', function (ws){
+    // News Connexion
+        console.log("Nouvelle connexion");
+        ws.send("Connexion établie.");
+    // Lancement Date loop for 10 Secondes
+        setInterval(() =>
+            ws.send(`${new Date()}`)
+        ,10 * 1000)
+    // Message Reçu
+        ws.on('message', function (message) {
+            console.log('received: %s', message)
+        })
+    // Request UserConnexion
+        ws.on('UserConnexion', (pseudo, mdp) => {
+            console.log('Pseudo: ' + pseudo);
+            console.log('Mot de passe: ' + mdp);
+            var result = "1";
+            if(pseudo == "woulfty" || mdp == "123"){
+                ws.send("ConnexionTrue", result)
+            }
+        });
 })
