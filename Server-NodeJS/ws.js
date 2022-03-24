7406f0a0e7d315eca44da9751767814ac84b20e6
Piebleu <napiebleu@gmail.com>
2022-03-24 09:55:37 +0100

    Ajouts commentaires.

--- Server-NodeJS/ws.js
+++ Server-NodeJS/ws.js
@@ -1,24 +1,27 @@
-var WebSocketServer = require('ws').Server,
-  wss = new WebSocketServer({port: 40510})
+// Création WebSocketServer
+    var WebSocketServer = require('ws').Server;
+    wss = new WebSocketServer({port: 40510});
 
-wss.on('connection', function (ws) {
-  ws.on('message', function (message) {
-    console.log('received: %s', message)
-  })
-
-  ws.on('UserConnexion', (pseudo, mdp) => {
-    console.log('Pseudo: ' + pseudo);
-    console.log('Mot de passe: ' + mdp);
-
-    var result = "1";
-    
-    if(pseudo == "woulfty" || mdp == "123"){
-      ws.send("ConnectionTrue", result)
-    }
-  });
-  
-  setInterval(
-    () => ws.send(`${new Date()}`),
-    2000
-  )
-})
+// Lancement WebSocketServer
+    wss.on('connection', function (ws){
+        // News Connexion
+            console.log("Nouvelle connexion");
+            ws.send("Connexion établie.");
+        // Lancement Date loop for 10 Secondes
+            setInterval(() =>
+                ws.send(`${new Date()}`)
+            ,10 * 1000)
+        // Message Reçu
+            ws.on('message', function (message) {
+                console.log('received: %s', message)
+            })
+        // Request UserConnexion
+            ws.on('UserConnexion', (pseudo, mdp) => {
+                console.log('Pseudo: ' + pseudo);
+                console.log('Mot de passe: ' + mdp);
+                var result = "1";
+                if(pseudo == "woulfty" || mdp == "123"){
+                    ws.send("ConnexionTrue", result)
+                }
+            });
+    })
