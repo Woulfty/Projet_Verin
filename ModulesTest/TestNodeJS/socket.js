4d257b549d02ef3d9b21987c6c3800a6a954e6cb
Piebleu <napiebleu@gmail.com>
2022-03-25 10:51:29 +0100

    Modification Module de test : Interprétation Réponse.

--- ModulesTest/TestNodeJS/socket.js
+++ ModulesTest/TestNodeJS/socket.js
@@ -1,13 +1,9 @@
 var ws = new WebSocket("ws://192.168.65.44:40510");
+
 ws.onopen = function () {
     console.log('websocket is connected ...')
     ws.send('connected')
 }
-ws.onmessage = function (ev) {
-    console.log(ev);
-}
-
-
-form.addEventListener('submit', function(e) {
-
-});
+ws.addEventListener("message", async (event, isBinary ) => {
+    console.log( event.data )
+})
