11acd139ab2d127c415f6b1212d3ea5f89e0b0db
Piebleu <napiebleu@gmail.com>
2022-03-25 10:53:18 +0100

    Mise en fonction WebSocket et MySQL2. Implémentation MYSQL en ASYNC. Réponse Websocket en ASYNC. Mise en fonction ListAffaires. Retrait Fonctions inutiles.

--- Server-NodeJS/ws.js
+++ Server-NodeJS/ws.js
@@ -1,68 +1,44 @@
-// Création WebSocketServer
-    var WebSocketServer = require('ws').Server;
-    wss = new WebSocketServer({port: 40510});
+const WebSocketServer = require('ws').Server;
+const mysql = require('mysql2/promise');
+
+(async() => {
+    // Création WebSocketServer
+    const wss = new WebSocketServer({port: 40510});
     console.log("Serveur Lancé.");
 
-// Lancement WebSocketServer
-    wss.on('connection', function (ws){
-        // News Connexion
-            console.log("Nouvelle connexion");
-            ws.send("Connexion établie.");
-        // Lancement Date loop for 10 Secondes
-            setInterval(() =>
-                ws.send(`${new Date()}`)
-            ,10 * 1000)
-        // Message Reçu
-            ws.on('message', function (message) {
-                console.log('received: %s', message);
-            })
-        // Request
-            // Request UserConnexion
-                ws.on('UserConnexion', function (message){
-                    console.log('received: %s', message);
-                    ws.send("Connexion établie.");
-                });
-            // Request ListAffaires 
-                ws.on('ListAffaires', function (message){
-                    console.log('received: %s', message);
-                    ws.send("ListAffaires Reponse.");
-                })
-            // Request Calculs Rendements
-                ws.on('CalcRends', function (message){
-                    console.log('received: %s', message);
-                    ws.send("Connexion établie.");
-                })
-            // Request Enregistrement Essai
-                ws.on('CalcRends', function (message){
-                    console.log('received: %s', message);
-                    ws.send("Connexion établie.");
-                })
-            // Request BDD Exportation
-                ws.on('CalcRends', function (message){
-                    console.log('received: %s', message);
-                    ws.send("Connexion établie.");
-                })
-            // Request BDD Importation
-                ws.on('CalcRends', function (message){
-                    console.log('received: %s', message);
-                    ws.send("Connexion établie.");
-                })
-            // Request BDD Initialisation
-                ws.on('CalcRends', function (message){
-                    console.log('received: %s', message);
-                    ws.send("Connexion établie.");
-                })
-    })
+    try{
+        var con = await mysql.createConnection({
+            host:"192.168.65.20",
+            user:"root",
+            password:"root",
+            database : "verain"
+        });
+    }catch(error){
+        console.log("Erreure de connexion à la base de données", error);
+    }
 
-// Création MySQL
-    const mysql = require('mysql');
-    const db = mysql.createConnection({
-        host:"192.168.65.20",
-        user:"root",
-        password:"root"
+    // Lancement WebSocketServer
+    wss.on( 'connection', function (ws){
+        // News Connexion
+        console.log("Nouvelle connexion");
+        ws.send("Connexion établie.");
+        // Lancement Date loop for 10 Secondes
+        setInterval(() =>
+            ws.send(`Temps : ${new Date()}`)
+        ,30 * 1000)
+        // Message Reçu Répo
+        ws.on('message', async function(message){
+            // ListeAffaires
+            if(message.slice() == 'ListAffaires'){
+                console.log('ListAffaires : %s', message);
+                const[rows, fields] = await con.execute('SELECT `ID` FROM `Affaire`', []);
+                ws.send(JSON.stringify(rows))
+            }
+            // Autres
+            else{
+                console.log('Reçu (Inconnu) : %s', message);
+                ws.send('Erreur : reçu inconnu : ' + message)
+            }
+        })
     });
-// Connexion BDD
-    db.connect(function(err) {
-        if (err) throw err;
-        console.log("Connecté à la base de données MySQL!");
-    });
+})();
