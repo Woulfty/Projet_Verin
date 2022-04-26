fd9bfa343625764fcb25c4ffcce20a036d978925
Piebleu <napiebleu@gmail.com>
2022-04-26 11:45:53 +0200

    Changement BDD utilisé.

--- Server-NodeJS/BDD_files/BDD_Default.sql
+++ Server-NodeJS/BDD_files/BDD_Default.sql
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de données : `verain`
+-- Base de données : `Verin`
 --
 
 -- --------------------------------------------------------
