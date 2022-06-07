d5b47ec24d9d82cd754a97fd7e26cc10a8a65f3a
Piebleu <napiebleu@gmail.com>
2022-06-07 11:39:23 +0200

    Mise à jours.

--- Server-NodeJS/BDD_files/BDD_Export.sql
+++ Server-NodeJS/BDD_files/BDD_Export.sql
@@ -19,7 +19,7 @@ CREATE TABLE IF NOT EXISTS `Affaire` (
   `Date` datetime NOT NULL DEFAULT current_timestamp(),
   PRIMARY KEY (`idAffaire`),
   KEY `PV` (`PV`)
-) ENGINE = InnoDB AUTO_INCREMENT = 17 DEFAULT CHARSET = utf8mb4;
+) ENGINE = InnoDB AUTO_INCREMENT = 21 DEFAULT CHARSET = utf8mb4;
 
 # ------------------------------------------------------------
 # SCHEMA DUMP FOR TABLE: Essaie
@@ -34,7 +34,7 @@ CREATE TABLE IF NOT EXISTS `Essaie` (
   PRIMARY KEY (`idEssaie`),
   KEY `Affaire` (`idAffaire`),
   CONSTRAINT `Essaie_ibfk_1` FOREIGN KEY (`idAffaire`) REFERENCES `Affaire` (`idAffaire`)
-) ENGINE = InnoDB AUTO_INCREMENT = 101 DEFAULT CHARSET = utf8mb4;
+) ENGINE = InnoDB AUTO_INCREMENT = 134 DEFAULT CHARSET = utf8mb4;
 
 # ------------------------------------------------------------
 # SCHEMA DUMP FOR TABLE: PV
@@ -48,10 +48,8 @@ CREATE TABLE IF NOT EXISTS `PV` (
   `Date` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
   PRIMARY KEY (`idPV`),
   KEY `User` (`idUser`),
-  KEY `Affaire` (`idAffaire`),
-  CONSTRAINT `PV_ibfk_1` FOREIGN KEY (`idUser`) REFERENCES `User` (`idUser`),
-  CONSTRAINT `PV_ibfk_2` FOREIGN KEY (`idAffaire`) REFERENCES `Affaire` (`idAffaire`)
-) ENGINE = InnoDB AUTO_INCREMENT = 26 DEFAULT CHARSET = utf8mb4;
+  KEY `Affaire` (`idAffaire`)
+) ENGINE = InnoDB AUTO_INCREMENT = 30 DEFAULT CHARSET = utf8mb4;
 
 # ------------------------------------------------------------
 # SCHEMA DUMP FOR TABLE: User
@@ -60,7 +58,7 @@ CREATE TABLE IF NOT EXISTS `PV` (
 CREATE TABLE IF NOT EXISTS `User` (
   `idUser` int(11) NOT NULL AUTO_INCREMENT,
   `Username` varchar(20) NOT NULL,
-  `Mdp` varchar(20) NOT NULL,
+  `Mdp` varchar(100) NOT NULL,
   PRIMARY KEY (`idUser`)
 ) ENGINE = InnoDB AUTO_INCREMENT = 12 DEFAULT CHARSET = utf8mb4;
 
@@ -68,16 +66,300 @@ CREATE TABLE IF NOT EXISTS `User` (
 # DATA DUMP FOR TABLE: Affaire
 # ------------------------------------------------------------
 
+INSERT INTO
+  `Affaire` (
+    `idAffaire`,
+    `Capteur`,
+    `Frequence`,
+    `TempAcquisition`,
+    `PV`,
+    `Date`
+  )
+VALUES
+  (1, 1, 6, 1000, 2, '2022-06-03 11:35:10');
+INSERT INTO
+  `Affaire` (
+    `idAffaire`,
+    `Capteur`,
+    `Frequence`,
+    `TempAcquisition`,
+    `PV`,
+    `Date`
+  )
+VALUES
+  (18, 1, 6, 1000, 2, '2022-06-03 13:21:15');
+INSERT INTO
+  `Affaire` (
+    `idAffaire`,
+    `Capteur`,
+    `Frequence`,
+    `TempAcquisition`,
+    `PV`,
+    `Date`
+  )
+VALUES
+  (19, 1, 6, 1000, 2, '2022-06-03 13:23:45');
+INSERT INTO
+  `Affaire` (
+    `idAffaire`,
+    `Capteur`,
+    `Frequence`,
+    `TempAcquisition`,
+    `PV`,
+    `Date`
+  )
+VALUES
+  (20, 1, 6, 1000, 2, '2022-06-03 13:29:42');
 
 # ------------------------------------------------------------
 # DATA DUMP FOR TABLE: Essaie
 # ------------------------------------------------------------
 
+INSERT INTO
+  `Essaie` (
+    `idEssaie`,
+    `idAffaire`,
+    `NumEssaie`,
+    `Debit`,
+    `Value`
+  )
+VALUES
+  (113, 1, 1, 82, 0.714928);
+INSERT INTO
+  `Essaie` (
+    `idEssaie`,
+    `idAffaire`,
+    `NumEssaie`,
+    `Debit`,
+    `Value`
+  )
+VALUES
+  (114, 1, 2, 81.24, 0.99775);
+INSERT INTO
+  `Essaie` (
+    `idEssaie`,
+    `idAffaire`,
+    `NumEssaie`,
+    `Debit`,
+    `Value`
+  )
+VALUES
+  (115, 1, 3, 88.07, 0.99215);
+INSERT INTO
+  `Essaie` (
+    `idEssaie`,
+    `idAffaire`,
+    `NumEssaie`,
+    `Debit`,
+    `Value`
+  )
+VALUES
+  (116, 1, 4, 88.07, 0.992159);
+INSERT INTO
+  `Essaie` (
+    `idEssaie`,
+    `idAffaire`,
+    `NumEssaie`,
+    `Debit`,
+    `Value`
+  )
+VALUES
+  (117, 18, 1, 88.07, 0.0785708);
+INSERT INTO
+  `Essaie` (
+    `idEssaie`,
+    `idAffaire`,
+    `NumEssaie`,
+    `Debit`,
+    `Value`
+  )
+VALUES
+  (118, 18, 2, 84.27, 1);
+INSERT INTO
+  `Essaie` (
+    `idEssaie`,
+    `idAffaire`,
+    `NumEssaie`,
+    `Debit`,
+    `Value`
+  )
+VALUES
+  (119, 18, 3, 164.75, 0.256753);
+INSERT INTO
+  `Essaie` (
+    `idEssaie`,
+    `idAffaire`,
+    `NumEssaie`,
+    `Debit`,
+    `Value`
+  )
+VALUES
+  (120, 18, 4, 666.59, -0.028089);
+INSERT INTO
+  `Essaie` (
+    `idEssaie`,
+    `idAffaire`,
+    `NumEssaie`,
+    `Debit`,
+    `Value`
+  )
+VALUES
+  (121, 18, 5, 605.86, 1.1115);
+INSERT INTO
+  `Essaie` (
+    `idEssaie`,
+    `idAffaire`,
+    `NumEssaie`,
+    `Debit`,
+    `Value`
+  )
+VALUES
+  (122, 19, 0, 79.72, 0.15991);
+INSERT INTO
+  `Essaie` (
+    `idEssaie`,
+    `idAffaire`,
+    `NumEssaie`,
+    `Debit`,
+    `Value`
+  )
+VALUES
+  (123, 19, 1, 176.14, 0.141729);
+INSERT INTO
+  `Essaie` (
+    `idEssaie`,
+    `idAffaire`,
+    `NumEssaie`,
+    `Debit`,
+    `Value`
+  )
+VALUES
+  (124, 19, 2, 194.36, 0.547804);
+INSERT INTO
+  `Essaie` (
+    `idEssaie`,
+    `idAffaire`,
+    `NumEssaie`,
+    `Debit`,
+    `Value`
+  )
+VALUES
+  (125, 19, 3, 214.86, -0.0268439);
+INSERT INTO
+  `Essaie` (
+    `idEssaie`,
+    `idAffaire`,
+    `NumEssaie`,
+    `Debit`,
+    `Value`
+  )
+VALUES
+  (126, 19, 4, 176.14, 0.994397);
+INSERT INTO
+  `Essaie` (
+    `idEssaie`,
+    `idAffaire`,
+    `NumEssaie`,
+    `Debit`,
+    `Value`
+  )
+VALUES
+  (127, 19, 5, 141.97, 0.116725);
+INSERT INTO
+  `Essaie` (
+    `idEssaie`,
+    `idAffaire`,
+    `NumEssaie`,
+    `Debit`,
+    `Value`
+  )
+VALUES
+  (128, 20, 0, 127.55, 0.996648);
+INSERT INTO
+  `Essaie` (
+    `idEssaie`,
+    `idAffaire`,
+    `NumEssaie`,
+    `Debit`,
+    `Value`
+  )
+VALUES
+  (129, 20, 1, 293.82, 0.552811);
+INSERT INTO
+  `Essaie` (
+    `idEssaie`,
+    `idAffaire`,
+    `NumEssaie`,
+    `Debit`,
+    `Value`
+  )
+VALUES
+  (130, 20, 2, 658.24, 0.666303);
+INSERT INTO
+  `Essaie` (
+    `idEssaie`,
+    `idAffaire`,
+    `NumEssaie`,
+    `Debit`,
+    `Value`
+  )
+VALUES
+  (131, 20, 3, 663.56, 0.145435);
+INSERT INTO
+  `Essaie` (
+    `idEssaie`,
+    `idAffaire`,
+    `NumEssaie`,
+    `Debit`,
+    `Value`
+  )
+VALUES
+  (132, 20, 4, 267.25, 1.512);
+INSERT INTO
+  `Essaie` (
+    `idEssaie`,
+    `idAffaire`,
+    `NumEssaie`,
+    `Debit`,
+    `Value`
+  )
+VALUES
+  (133, 20, 5, 268.76, 0.20787);
 
 # ------------------------------------------------------------
 # DATA DUMP FOR TABLE: PV
 # ------------------------------------------------------------
 
+INSERT INTO
+  `PV` (`idPV`, `idAffaire`, `idUser`, `Texte`, `Date`)
+VALUES
+  (
+    26,
+    19,
+    7,
+    '<img src=\"https://www.stampaprint.fr/blog/wp-content/uploads/2016/04/gif-esempio.gif\" alt=\"Grapefruit slice atop a pile of other slices\">\r\n\r\n',
+    '2022-06-07 10:59:30'
+  );
+INSERT INTO
+  `PV` (`idPV`, `idAffaire`, `idUser`, `Texte`, `Date`)
+VALUES
+  (
+    27,
+    19,
+    7,
+    '??????????????? ???? ',
+    '2022-06-03 13:30:13'
+  );
+INSERT INTO
+  `PV` (`idPV`, `idAffaire`, `idUser`, `Texte`, `Date`)
+VALUES
+  (
+    29,
+    20,
+    8,
+    'Probléme sur le verrin, t\'a fait péter le joint !\n<img src=\"http://3.bp.blogspot.com/-q-nJc6gdUhE/Um2r9ES-JQI/AAAAAAAAFzI/CjOEG8B8WNk/s1600/Principe_verin.gif\">',
+    '2022-06-07 10:54:49'
+  );
 
 # ------------------------------------------------------------
 # DATA DUMP FOR TABLE: User
@@ -86,23 +368,23 @@ CREATE TABLE IF NOT EXISTS `User` (
 INSERT INTO
   `User` (`idUser`, `Username`, `Mdp`)
 VALUES
-  (7, 'root', 'root');
+  (7, 'root', '63a9f0ea7bb98050796b649e85481845');
 INSERT INTO
   `User` (`idUser`, `Username`, `Mdp`)
 VALUES
-  (8, 'greg', 'greg');
+  (8, 'greg', 'ea26b0075d29530c636d6791bb5d73f4');
 INSERT INTO
   `User` (`idUser`, `Username`, `Mdp`)
 VALUES
-  (9, 'kylian', 'kiki');
+  (9, 'kylian', '0d61130a6dd5eea85c2c5facfe1c15a7');
 INSERT INTO
   `User` (`idUser`, `Username`, `Mdp`)
 VALUES
-  (10, 'nico', 'nico');
+  (10, 'Nicolas', 'deb97a759ee7b8ba42e02dddf2b412fe');
 INSERT INTO
   `User` (`idUser`, `Username`, `Mdp`)
 VALUES
-  (11, 'alex', 'alex');
+  (11, 'alex', '534b44a19bf18d20b71ecc4eb77c572f');
 
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
--- Server-NodeJS/BDD_files/BDD_Temps.sql
+++ Server-NodeJS/BDD_files/BDD_Temps.sql
@@ -1,66 +1,388 @@
--- phpMyAdmin SQL Dump
--- version 5.1.1
--- https://www.phpmyadmin.net/
---
--- Hôte : 127.0.0.1:3306
--- Généré le : mar. 18 jan. 2022 à 08:12
--- Version du serveur : 5.7.36
--- Version de PHP : 7.4.26
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+ 40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+/*!40101 SET NAMES utf8 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
---
--- Base de données : `utilisateurs`
---
+# ------------------------------------------------------------
+# SCHEMA DUMP FOR TABLE: Affaire
+# ------------------------------------------------------------
 
--- --------------------------------------------------------
+CREATE TABLE IF NOT EXISTS `Affaire` (
+  `idAffaire` int(11) NOT NULL AUTO_INCREMENT,
+  `Capteur` int(11) NOT NULL DEFAULT 1,
+  `Frequence` int(11) NOT NULL,
+  `TempAcquisition` int(11) NOT NULL,
+  `PV` int(11) NOT NULL,
+  `Date` datetime NOT NULL DEFAULT current_timestamp(),
+  PRIMARY KEY (`idAffaire`),
+  KEY `PV` (`PV`)
+) ENGINE = InnoDB AUTO_INCREMENT = 21 DEFAULT CHARSET = utf8mb4;
 
---
--- Structure de la table `user`
---
+# ------------------------------------------------------------
+# SCHEMA DUMP FOR TABLE: Essaie
+# ------------------------------------------------------------
 
-DROP TABLE IF EXISTS `user`;
-CREATE TABLE IF NOT EXISTS `user` (
-  `id` int(50) NOT NULL AUTO_INCREMENT,
-  `MDP` varchar(50) NOT NULL,
-  `pseudo` varchar(50) NOT NULL,
-  `admin` varchar(50) NOT NULL,
-  `pdp` varchar(5000) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `nom` (`pseudo`),
-  KEY `MDP` (`MDP`)
-) ENGINE=InnoDB AUTO_INCREMENT=90 DEFAULT CHARSET=utf8mb4;
+CREATE TABLE IF NOT EXISTS `Essaie` (
+  `idEssaie` int(11) NOT NULL AUTO_INCREMENT,
+  `idAffaire` int(11) NOT NULL,
+  `NumEssaie` int(11) NOT NULL,
+  `Debit` float NOT NULL,
+  `Value` float NOT NULL,
+  PRIMARY KEY (`idEssaie`),
+  KEY `Affaire` (`idAffaire`),
+  CONSTRAINT `Essaie_ibfk_1` FOREIGN KEY (`idAffaire`) REFERENCES `Affaire` (`idAffaire`)
+) ENGINE = InnoDB AUTO_INCREMENT = 134 DEFAULT CHARSET = utf8mb4;
 
---
--- Déchargement des données de la table `user`
---
+# ------------------------------------------------------------
+# SCHEMA DUMP FOR TABLE: PV
+# ------------------------------------------------------------
 
-INSERT INTO `user` (`id`, `MDP`, `pseudo`, `admin`, `pdp`) VALUES
-(40, '23w9j4', 'Asgarrrr', 'true', 'default.png'),
-(47, 'la main', 'lucum', 'true', 'default.png'),
-(48, '123', 'Woulfty', 'true', 'default.png'),
-(60, '123456789', 'Admin', 'false ', 'default.png'),
-(69, '123', 'lea', 'false', 'default.png'),
-(70, '456', 'lolo', 'false', 'default.png'),
-(79, 'le lapin', 'pinpin', 'false', 'default.jpg'),
-(80, '123', 'Julien', 'false', 'default.png'),
-(81, 'pxcleme', 'pxcleme', 'false', 'default.png'),
-(82, '33', 'alex', 'false', 'default.png'),
-(83, '123', 'UwU', 'false', 'default.png'),
-(84, '030120', 'julien0301', 'false', 'default.png'),
-(85, 'Hentai', 'Zerzeusse', 'false', 'default.png'),
-(88, '123', 'conan', 'false', 'default.png'),
-(89, 'weshlesgars', 'Louis', 'false', 'default.png');
-COMMIT;
+CREATE TABLE IF NOT EXISTS `PV` (
+  `idPV` int(11) NOT NULL AUTO_INCREMENT,
+  `idAffaire` int(11) NOT NULL,
+  `idUser` int(11) NOT NULL,
+  `Texte` varchar(1000) NOT NULL,
+  `Date` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  PRIMARY KEY (`idPV`),
+  KEY `User` (`idUser`),
+  KEY `Affaire` (`idAffaire`)
+) ENGINE = InnoDB AUTO_INCREMENT = 29 DEFAULT CHARSET = utf8mb4;
 
+# ------------------------------------------------------------
+# SCHEMA DUMP FOR TABLE: User
+# ------------------------------------------------------------
+
+CREATE TABLE IF NOT EXISTS `User` (
+  `idUser` int(11) NOT NULL AUTO_INCREMENT,
+  `Username` varchar(20) NOT NULL,
+  `Mdp` varchar(100) NOT NULL,
+  PRIMARY KEY (`idUser`)
+) ENGINE = InnoDB AUTO_INCREMENT = 12 DEFAULT CHARSET = utf8mb4;
+
+# ------------------------------------------------------------
+# DATA DUMP FOR TABLE: Affaire
+# ------------------------------------------------------------
+
+INSERT INTO
+  `Affaire` (
+    `idAffaire`,
+    `Capteur`,
+    `Frequence`,
+    `TempAcquisition`,
+    `PV`,
+    `Date`
+  )
+VALUES
+  (1, 1, 6, 1000, 2, '2022-06-03 11:35:10');
+INSERT INTO
+  `Affaire` (
+    `idAffaire`,
+    `Capteur`,
+    `Frequence`,
+    `TempAcquisition`,
+    `PV`,
+    `Date`
+  )
+VALUES
+  (18, 1, 6, 1000, 2, '2022-06-03 13:21:15');
+INSERT INTO
+  `Affaire` (
+    `idAffaire`,
+    `Capteur`,
+    `Frequence`,
+    `TempAcquisition`,
+    `PV`,
+    `Date`
+  )
+VALUES
+  (19, 1, 6, 1000, 2, '2022-06-03 13:23:45');
+INSERT INTO
+  `Affaire` (
+    `idAffaire`,
+    `Capteur`,
+    `Frequence`,
+    `TempAcquisition`,
+    `PV`,
+    `Date`
+  )
+VALUES
+  (20, 1, 6, 1000, 2, '2022-06-03 13:29:42');
+
+# ------------------------------------------------------------
+# DATA DUMP FOR TABLE: Essaie
+# ------------------------------------------------------------
+
+INSERT INTO
+  `Essaie` (
+    `idEssaie`,
+    `idAffaire`,
+    `NumEssaie`,
+    `Debit`,
+    `Value`
+  )
+VALUES
+  (113, 1, 1, 82, 0.714928);
+INSERT INTO
+  `Essaie` (
+    `idEssaie`,
+    `idAffaire`,
+    `NumEssaie`,
+    `Debit`,
+    `Value`
+  )
+VALUES
+  (114, 1, 2, 81.24, 0.99775);
+INSERT INTO
+  `Essaie` (
+    `idEssaie`,
+    `idAffaire`,
+    `NumEssaie`,
+    `Debit`,
+    `Value`
+  )
+VALUES
+  (115, 1, 3, 88.07, 0.99215);
+INSERT INTO
+  `Essaie` (
+    `idEssaie`,
+    `idAffaire`,
+    `NumEssaie`,
+    `Debit`,
+    `Value`
+  )
+VALUES
+  (116, 1, 4, 88.07, 0.992159);
+INSERT INTO
+  `Essaie` (
+    `idEssaie`,
+    `idAffaire`,
+    `NumEssaie`,
+    `Debit`,
+    `Value`
+  )
+VALUES
+  (117, 18, 1, 88.07, 0.0785708);
+INSERT INTO
+  `Essaie` (
+    `idEssaie`,
+    `idAffaire`,
+    `NumEssaie`,
+    `Debit`,
+    `Value`
+  )
+VALUES
+  (118, 18, 2, 84.27, 1);
+INSERT INTO
+  `Essaie` (
+    `idEssaie`,
+    `idAffaire`,
+    `NumEssaie`,
+    `Debit`,
+    `Value`
+  )
+VALUES
+  (119, 18, 3, 164.75, 0.256753);
+INSERT INTO
+  `Essaie` (
+    `idEssaie`,
+    `idAffaire`,
+    `NumEssaie`,
+    `Debit`,
+    `Value`
+  )
+VALUES
+  (120, 18, 4, 666.59, -0.028089);
+INSERT INTO
+  `Essaie` (
+    `idEssaie`,
+    `idAffaire`,
+    `NumEssaie`,
+    `Debit`,
+    `Value`
+  )
+VALUES
+  (121, 18, 5, 605.86, 1.1115);
+INSERT INTO
+  `Essaie` (
+    `idEssaie`,
+    `idAffaire`,
+    `NumEssaie`,
+    `Debit`,
+    `Value`
+  )
+VALUES
+  (122, 19, 0, 79.72, 0.15991);
+INSERT INTO
+  `Essaie` (
+    `idEssaie`,
+    `idAffaire`,
+    `NumEssaie`,
+    `Debit`,
+    `Value`
+  )
+VALUES
+  (123, 19, 1, 176.14, 0.141729);
+INSERT INTO
+  `Essaie` (
+    `idEssaie`,
+    `idAffaire`,
+    `NumEssaie`,
+    `Debit`,
+    `Value`
+  )
+VALUES
+  (124, 19, 2, 194.36, 0.547804);
+INSERT INTO
+  `Essaie` (
+    `idEssaie`,
+    `idAffaire`,
+    `NumEssaie`,
+    `Debit`,
+    `Value`
+  )
+VALUES
+  (125, 19, 3, 214.86, -0.0268439);
+INSERT INTO
+  `Essaie` (
+    `idEssaie`,
+    `idAffaire`,
+    `NumEssaie`,
+    `Debit`,
+    `Value`
+  )
+VALUES
+  (126, 19, 4, 176.14, 0.994397);
+INSERT INTO
+  `Essaie` (
+    `idEssaie`,
+    `idAffaire`,
+    `NumEssaie`,
+    `Debit`,
+    `Value`
+  )
+VALUES
+  (127, 19, 5, 141.97, 0.116725);
+INSERT INTO
+  `Essaie` (
+    `idEssaie`,
+    `idAffaire`,
+    `NumEssaie`,
+    `Debit`,
+    `Value`
+  )
+VALUES
+  (128, 20, 0, 127.55, 0.996648);
+INSERT INTO
+  `Essaie` (
+    `idEssaie`,
+    `idAffaire`,
+    `NumEssaie`,
+    `Debit`,
+    `Value`
+  )
+VALUES
+  (129, 20, 1, 293.82, 0.552811);
+INSERT INTO
+  `Essaie` (
+    `idEssaie`,
+    `idAffaire`,
+    `NumEssaie`,
+    `Debit`,
+    `Value`
+  )
+VALUES
+  (130, 20, 2, 658.24, 0.666303);
+INSERT INTO
+  `Essaie` (
+    `idEssaie`,
+    `idAffaire`,
+    `NumEssaie`,
+    `Debit`,
+    `Value`
+  )
+VALUES
+  (131, 20, 3, 663.56, 0.145435);
+INSERT INTO
+  `Essaie` (
+    `idEssaie`,
+    `idAffaire`,
+    `NumEssaie`,
+    `Debit`,
+    `Value`
+  )
+VALUES
+  (132, 20, 4, 267.25, 1.512);
+INSERT INTO
+  `Essaie` (
+    `idEssaie`,
+    `idAffaire`,
+    `NumEssaie`,
+    `Debit`,
+    `Value`
+  )
+VALUES
+  (133, 20, 5, 268.76, 0.20787);
+
+# ------------------------------------------------------------
+# DATA DUMP FOR TABLE: PV
+# ------------------------------------------------------------
+
+INSERT INTO
+  `PV` (`idPV`, `idAffaire`, `idUser`, `Texte`, `Date`)
+VALUES
+  (
+    26,
+    19,
+    7,
+    ' \t? \t? \t? \t? \t? \t? \t? \t? \t?',
+    '2022-06-03 13:29:26'
+  );
+INSERT INTO
+  `PV` (`idPV`, `idAffaire`, `idUser`, `Texte`, `Date`)
+VALUES
+  (
+    27,
+    19,
+    7,
+    '??????????????? ???? ',
+    '2022-06-03 13:30:13'
+  );
+INSERT INTO
+  `PV` (`idPV`, `idAffaire`, `idUser`, `Texte`, `Date`)
+VALUES
+  (28, 20, 8, 'Bravo a tous !', '2022-06-03 13:32:52');
+
+# ------------------------------------------------------------
+# DATA DUMP FOR TABLE: User
+# ------------------------------------------------------------
+
+INSERT INTO
+  `User` (`idUser`, `Username`, `Mdp`)
+VALUES
+  (7, 'root', '63a9f0ea7bb98050796b649e85481845');
+INSERT INTO
+  `User` (`idUser`, `Username`, `Mdp`)
+VALUES
+  (8, 'greg', 'ea26b0075d29530c636d6791bb5d73f4');
+INSERT INTO
+  `User` (`idUser`, `Username`, `Mdp`)
+VALUES
+  (9, 'kylian', '0d61130a6dd5eea85c2c5facfe1c15a7');
+INSERT INTO
+  `User` (`idUser`, `Username`, `Mdp`)
+VALUES
+  (10, 'Nicolas', 'deb97a759ee7b8ba42e02dddf2b412fe');
+INSERT INTO
+  `User` (`idUser`, `Username`, `Mdp`)
+VALUES
+  (11, 'alex', '534b44a19bf18d20b71ecc4eb77c572f');
+
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
